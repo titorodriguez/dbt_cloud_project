f63cd6c74dafa3e375a0d6c8e5f5259d78b369e9
Tito Rodriguez <arodriguezcrz@gmail.com>
2025-08-31 03:00:05 +0000

    Adding documentation, standardize sources and models

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,15 +1,15 @@
 with orders as(
-    select * from {{ ref("stg_jaffle_shop_orders") }}
+    select * from {{ ref("stg_jaffle_shop__orders") }}
 ),
 
 payments as(
-    select * from {{ ref("stg_stripe__payments") }}
+    select * from {{ ref("stg_stripe__payment") }}
 ),
 
 order_payments as(
     select
         order_id,
-        sum(case when status = 'success' then amount end) as amount
+        sum(case when payment_status = 'success' then amount end) as amount
     from payments
     group by 1
 ),
--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -1,5 +1,13 @@
-select
-    id as customer_id,
-    first_name,
-    last_name
-from {{ source('jaffle_shop', 'customers') }}
+with source as (
+    select * from {{ source('jaffle_shop', 'customers') }}
+),
+
+renamed as (
+    select
+        id as customer_id,
+        first_name,
+        last_name
+    from source
+)
+
+select * from renamed
